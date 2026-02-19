c0b6e59d2e412b2654b5daa7ffb6b8bdd4b4de7f
abdulkarim41 <abdulkarim.jatri@gmail.com>
2026-02-19 11:48:45 +0600

    chore(ui): improve generics in RecyclerView extension
    This commit refactors the `setUpVerticalRecyclerView` extension function in the `core:ui` module to use more descriptive and conventional generic type parameters.
    
    The generic parameter `viewHolder` has been renamed to `VH` and constrained to `RecyclerView.ViewHolder` for better clarity and adherence to Kotlin's style guide.

--- core/ui/src/main/kotlin/com/abdulkarim/ui/extfun/RecyclerViewExtension.kt
+++ core/ui/src/main/kotlin/com/abdulkarim/ui/extfun/RecyclerViewExtension.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-fun<viewHolder,T:RecyclerView.Adapter<viewHolder>> Context.setUpVerticalRecyclerView(view: RecyclerView,viewAdapter:T){
+fun <VH : RecyclerView.ViewHolder, T : RecyclerView.Adapter<VH>> Context.setUpVerticalRecyclerView(
+    view: RecyclerView,
+    viewAdapter: T
+) {
     view.layoutManager = LinearLayoutManager(this)
     view.adapter = viewAdapter
 }
